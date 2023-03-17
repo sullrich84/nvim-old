0c9758bf23dfba00af1c6bccaaba77a8a707e42b
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-03-17 09:15:40 +0100

    chore: cleanup configs

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,13 +5,14 @@ return {
       bind_to_cwd = false,
       follow_current_file = true,
       filtered_items = {
-        visible = true,
+        visible = false,
         hide_dotfiles = false,
         hide_gitignored = false,
         hide_by_name = {
           ".DS_Store",
           "thumbs.db",
           "node_modules",
+          ".git",
         },
       },
     },
