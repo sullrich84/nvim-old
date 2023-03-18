54a7e54d81980cdb8954724d7bed93d1f3287093
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-03-18 01:41:39 +0100

    chore: reorganize dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -17,16 +17,15 @@ return {
              ⠀⠀⠛⠿⠿⠃⠀⠻⠿⠋⠀⠀⠻⠇⠘⠿⠿⠟⠀sully.vim
 ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
   ]]
-
+      ---@diagnostic disable-next-line: missing-parameter
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("p", " " .. " Projects", ":Telescope project <CR>"),
-        dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+        --dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
         --dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
         dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
         dashboard.button("l", " " .. " Lazy", ":Lazy<CR>"),
         dashboard.button("h", " " .. " Check Health", ":checkhealth<CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
