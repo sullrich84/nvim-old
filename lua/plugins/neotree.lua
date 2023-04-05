38bb7afd210f82a9882d3cb0b32613611e4d2bc9
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-05 10:59:12 +0200

    chore: update nerd-tree icons

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,9 +4,41 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
+    default_component_configs = {
+      icon = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "",
+      },
+      modified = {
+        symbol = "",
+        highlight = "NeoTreeModified",
+      },
+      name = {
+        trailing_slash = false,
+        use_git_status_colors = true,
+        highlight = "NeoTreeFileName",
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "",
+          modified = "",
+          deleted = "✖",
+          renamed = "",
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "",
+          staged = "",
+          conflict = "",
+        },
+      },
+    },
     filesystem = {
       bind_to_cwd = false,
       follow_current_file = true,
+
       filtered_items = {
         visible = false,
         hide_dotfiles = false,
