8d96278fd9b9db290fba41fc8a6ebbcdc13ec779
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-06 22:19:24 +0200

    chore: fix dependency issue

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,16 +1,21 @@
 return {
   "vim-test/vim-test",
   lazy = false,
+  dependencies = {
+    "folke/which-key.nvim",
+    "akinsho/toggleterm.nvim",
+  },
   keys = function()
-    local wk = require("which-key")
-    wk.register({
-      t = {
-        name = "Tests",
-        n = { "<cmd>TestNearest<cr>", "Run nearest test" },
-        f = { "<cmd>TestFile<cr>", "Run test file" },
-        l = { "<cmd>TestLast<cr>", "Rerun latest test" },
-        v = { "<cmd>TestVisit<cr>", "Visit test file" },
-      },
-    }, { prefix = "<leader>c" })
+    if pcall(require, "which-key") then
+      require("which-key").register({
+        t = {
+          name = "Tests",
+          n = { "<cmd>TestNearest -strategy=toggleterm<cr>", "Run nearest test" },
+          f = { "<cmd>TestFile -strategy=toggleterm<cr>", "Run test file" },
+          l = { "<cmd>TestLast -strategy=toggleterm<cr>", "Rerun latest test" },
+          v = { "<cmd>TestVisit -strategy=toggleterm<cr>", "Visit test file" },
+        },
+      }, { prefix = "<leader>c" })
+    end
   end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- add tsx and treesitter
     vim.list_extend(opts.ensure_installed, {
       "tsx",
       "bash",
