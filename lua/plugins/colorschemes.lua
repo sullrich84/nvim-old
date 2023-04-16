ae489bc8cc7be5341df286d341c402311b630bb7
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-16 23:55:16 +0200

    feat: add color themes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,6 +1,9 @@
 return {
   "LazyVim/LazyVim",
   dependencies = {
+    { "rebelot/kanagawa.nvim", name = "kanagawa", lazy = false, priority = 1000 },
+    { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
+    { "EdenEast/nightfox.nvim", name = "nightfox", lazy = false, priority = 1000 },
     { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
   },
   opts = {
