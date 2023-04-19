26009293e91dc0f9d5c7208bd8350dd42c686b25
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-19 23:49:21 +0200

    chore: update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,6 +3,8 @@ return {
     "goolord/alpha-nvim",
     opts = function()
       local dashboard = require("alpha.themes.dashboard")
+
+      -- HEADER
       local logo = [[
              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣤⣴⡶⠖⢀⣀⣤⣤⣤⣀
              ⠀⠀⠀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⣿⠛⣡⣴⣾⣿⣿⣿⣿⣿⣿⣷⡀
@@ -16,8 +18,10 @@ return {
              ⠀⠀⣿⣿⣿⣿⠀⣿⣿⣷⠀⠀⣿⡆⢸⣿⣿⣿⠀⠀⠀⠀⠉⠉⠁
              ⠀⠀⠛⠿⠿⠃⠀⠻⠿⠋⠀⠀⠻⠇⠘⠿⠿⠟⠀sully.vim
 ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
-  ]]
-      ---@diagnostic disable-next-line: missing-parameter
+      ]]
+      dashboard.section.header.opts.hl = "Identifier"
+
+      -- BUTTONS
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
         dashboard.button("p", " " .. " Projects", ":Telescope project <CR>"),
@@ -30,14 +34,15 @@ return {
         dashboard.button("h", " " .. " Check Health", ":checkhealth<CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
       for _, button in ipairs(dashboard.section.buttons.val) do
         button.opts.hl = "AlphaButtons"
         button.opts.hl_shortcut = "AlphaShortcut"
       end
+
       dashboard.section.footer.opts.hl = "Type"
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
       dashboard.opts.layout[1].val = 8
+
       return dashboard
     end,
   },
