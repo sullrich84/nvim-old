589488a8e9e7aa826b425612aabba51282d41d0f
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-22 22:34:31 +0200

    feat: change colortheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -7,6 +7,6 @@ return {
     { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
   },
   opts = {
-    colorscheme = "nightfly",
+    colorscheme = "carbonfox",
   },
 }
