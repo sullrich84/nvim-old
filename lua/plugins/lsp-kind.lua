40c821c45b21c228f0a6a7ad32d8ed4601c10db8
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-23 12:59:05 +0200

    feat: config pylsp

--- lua/plugins/lsp-kind.lua
+++ lua/plugins/lsp-kind.lua
@@ -10,7 +10,7 @@ return {
           Function = " ",
           Constructor = " ",
           Field = " ",
-          Variable = " ",
+          Variable = "󰫧 ",
           Class = " ",
           Interface = " ",
           Module = " ",
