f1b001e03637a70f1a6c04cfc047de40a75b544a
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-23 12:59:33 +0200

    feat: disable notify

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,16 @@
+return {
+  "rcarriga/nvim-notify",
+  enabled = false,
+  opts = {
+    animation = "fade_in_slide_out",
+    render = "compact",
+    top_down = false,
+    icons = {
+      ERROR = " ",
+      WARN = " ",
+      INFO = " ",
+      DEBUG = " ",
+      TRACE = " ",
+    },
+  }
+}
