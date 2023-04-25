f2460c2f9929f4fae7740795502ce336108bd6cf
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-04-26 00:40:24 +0200

    chore: fix dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,11 +1,4 @@
-return {
-  {
-    "goolord/alpha-nvim",
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-
-      -- HEADER
-      local logo = [[
+local logo = [[
              ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣤⣴⡶⠖⢀⣀⣤⣤⣤⣀
              ⠀⠀⠀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⣿⠛⣡⣴⣾⣿⣿⣿⣿⣿⣿⣷⡀
              ⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧
@@ -18,18 +11,26 @@ return {
              ⠀⠀⣿⣿⣿⣿⠀⣿⣿⣷⠀⠀⣿⡆⢸⣿⣿⣿⠀⠀⠀⠀⠉⠉⠁
              ⠀⠀⠛⠿⠿⠃⠀⠻⠿⠋⠀⠀⠻⠇⠘⠿⠿⠟⠀sully.vim
 ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
-      ]]
+]]
+
+return {
+  {
+    "goolord/alpha-nvim",
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- HEADER
       dashboard.section.header.opts.hl = "Keyword"
 
       -- BUTTONS
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
         dashboard.button("p", " " .. " Projects", ":Telescope project <CR>"),
-        --dashboard.button("f", "󰍉 " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        --dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+        dashboard.button("r", " " .. " Recent Files", ":Telescope oldfiles <CR>"),
+        dashboard.button("g", " " .. " Live Grep", ":Telescope live_grep <CR>"),
+        dashboard.button("f", "󰍉 " .. " Find File", ":Telescope find_files <CR>"),
+        dashboard.button("s", " " .. " Session: Restore Session", [[:lua require("persistence").load() <cr>]]),
+        dashboard.button("c", " " .. " init.lua", ":e $MYVIMRC <CR>"),
         dashboard.button("l", " " .. " Lazy", ":Lazy<CR>"),
         dashboard.button("h", " " .. " Check Health", ":checkhealth<CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
@@ -41,9 +42,9 @@ return {
       end
 
       dashboard.section.footer.opts.hl = "Type"
-      dashboard.opts.layout[1].val = 8
+      dashboard.opts.layout[1].val = 3
 
       return dashboard
     end,
-  },
+  }
 }
